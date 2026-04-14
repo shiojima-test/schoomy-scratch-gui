1badf6592b66b7f509e9c3eb6ddf411df78477ef
shiojima-test <contact.schoomy@gmail.com>
2026-04-15 01:01:54 +0900

    webpack.config.js タイトルをスクーミーワールドスクラッチに変更

--- webpack.config.js
+++ webpack.config.js
@@ -137,14 +137,14 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui.min'],
                 template: 'src/playground/index.ejs',
-                title: 'E羊icques: A Scratch mod with a custom aspect ratio that can load an extension from a URL',
+                title: 'スクーミーワールドスクラッチ',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'flags.min'],
                 template: 'src/playground/index.ejs',
                 filename: 'flags.html',
-                title: 'E羊icques: URL settings'
+                title: 'スクーミーワールドスクラッチ: URL設定'
             }),
             new CopyWebpackPlugin([{
                 from: 'static',
